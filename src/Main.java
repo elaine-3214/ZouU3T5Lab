bf47b7e53683ce7b0e90791d1844c75e3edf1b0d
1e10laptop <sample@bths.edu>
2022-10-31 12:25:29 -0400

    final

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+
+
+// CODE SEGMENT D
+        int p = 5;
+        int q = 0;
+
+        if (p > 0 && (p / q) == 2)
+        {
+            System.out.println("it’s true :)");
+        }
+        else
+        {
+            System.out.println("it’s false :(");
+        }
+
+        myScanner.close();
+    }
+}
